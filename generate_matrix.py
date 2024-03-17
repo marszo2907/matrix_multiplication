c377e32863d3bbc128693cb64705a238fc3d4d14
Marcin Szostak <m.szostak2907@icloud.com>
2024-03-17 11:08:09 +0100

    Replaced --random argument with a flag

--- generate_matrix.py
+++ generate_matrix.py
@@ -6,14 +6,15 @@ def main():
                                      description="The script generates a specified matrix and saves it to a specified file")
     parser.add_argument("-s", "--size", type=int, required=True)
     parser.add_argument("-o", "--output", required=True)
-    parser.add_argument("-r", "--random", type=bool, default=False)
+    parser.add_argument("-r", "--random", action="store_true", default=False)
     args = vars(parser.parse_args())
     
-    file = open(args["output"] + ".txt", "w")
+    file_name = args["output"] if args["output"].endswith(".txt") else args["output"] + ".txt"
+    file = open(file_name, "w")
     file.write(f"{args['size']} {args['size']}\n")
     for row in range(args["size"]):
         for column in range(args["size"]):
-            if (args["random"]):
+            if args["random"]:
                 file.write(f"{random.randint(0, 9)} ")
             else:
                 file.write("1 ")
