1e8f9bf13b1169de55d91281c7413321d78d2cb6
Marcin Szostak <m.szostak2907@icloud.com>
2024-03-17 10:58:17 +0100

    Added script to generate matrix input files

---
+++ generate_matrix.py
@@ -0,0 +1,24 @@
+import argparse
+import random
+
+def main():
+    parser = argparse.ArgumentParser(prog="generate_matrix.py",
+                                     description="The script generates a specified matrix and saves it to a specified file")
+    parser.add_argument("-s", "--size", type=int, required=True)
+    parser.add_argument("-o", "--output", required=True)
+    parser.add_argument("-r", "--random", type=bool, default=False)
+    args = vars(parser.parse_args())
+    
+    file = open(args["output"] + ".txt", "w")
+    file.write(f"{args['size']} {args['size']}\n")
+    for row in range(args["size"]):
+        for column in range(args["size"]):
+            if (args["random"]):
+                file.write(f"{random.randint(0, 9)} ")
+            else:
+                file.write("1 ")
+        file.write("\n")
+    file.close()
+
+if __name__ == "__main__":
+    main()
